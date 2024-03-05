6e7fd2ad6202f34464f77e6375df77245b8705d9
Muniza <159540004+Muniza-NAbeel@users.noreply.github.com>
2024-03-05 14:23:37 +0500

    Add files via upload

---
+++ Q3.js
@@ -0,0 +1,7 @@
+"use strict";
+// 3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+Object.defineProperty(exports, "__esModule", { value: true });
+let name1 = "Muniza Nabeel";
+console.log(name1.toLowerCase());
+console.log(name1.toUpperCase());
+console.log(name1.charAt(0).toUpperCase() + name1.slice(1));
